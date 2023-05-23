edc7166625eafeeaddd22ac8e2a8dd395bb141b6
Jorgetorreslopez <usda4ever@gmail.com>
2023-05-23 19:37:54 -0400

    Greeting express application created.

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get('/greeting', (req, res) => {
     res.send('<h1>Hello Stranger</h1>')
 })
 
+app.get('/greeting/:name', (req, res) => {
+    const name = req.params.name;
+    const greeting = `Yo ${name} whats good!`;
+    res.send(greeting)
+})
+
 
 app.listen(port, () => {
     console.log(`listening on ${port}`)
