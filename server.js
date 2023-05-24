08d0c22776e72bcd8bf1d4289b9693a6e79355b2
Jorgetorreslopez <usda4ever@gmail.com>
2023-05-23 22:03:26 -0400

    Fibonacci.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 3002;
 
 const app = express();
 
+
 app.get("/greeting", (req, res) => {
   res.send("<h1>Hello Stranger</h1>");
 });
@@ -53,6 +54,35 @@ app.get("/magic/:question", (req, res) => {
   res.send(trueAnswer)
 
 });
+
+
+//Fibonacci
+app.get('/fibonacci/:number', (req, res) => {
+    const urlNumber = parseInt(req.params.number)
+
+    // Used the logic from this video https://www.youtube.com/watch?v=nhv3qQx8Yt0&t=156s
+    function checkFibonacci(num) {
+        let n1 = 0
+        let n2 = 1
+        let n3 = n1 + n2
+    
+        while (n3 <= num) {
+
+            if (num === n3) {
+                return '<h1>Very Good. It is Fibonacci</1>';
+            }
+            n1 = n2
+            n2 = n3
+            n3 = n1 + n2
+        }
+        return '<h1>THIS.IS.NOT.FIB!!!</h1>'
+    }
+
+    res.send(checkFibonacci(urlNumber))
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
